f362be978749e7baa5733b51479a007566accb6c
Paula <giannajuncal@gmail.com>
2023-02-06 14:01:55 +0000

    Backend del login terminado

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,9 +4,26 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
+
+db = SQLAlchemy()
 
 api = Blueprint('api', __name__)
 
+
+@api.route('/user', methods=['GET'])
+def handle_user():
+    
+    alluser = User.query.all()
+    results = list(map(lambda item: item.serialize(),alluser))
+
+    return jsonify(results), 200
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -16,3 +33,31 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    user= User.query.filter_by(email=email).first()
+
+
+    if email != user.email or password != user.password:
+        return jsonify({"msg": "Bad email or password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+
+
+
+
+
+    if __name__ == "__main__":
+        app.run()
+# ____________________________________
+
+if __name__ == '__main__':
+    PORT = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=PORT, debug=False)
