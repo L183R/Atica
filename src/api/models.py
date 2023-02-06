197ba1b4f5e536e8b56a590cf8ed81a5fc906b52
Paula <giannajuncal@gmail.com>
2023-02-06 19:28:19 +0000

    Coso con register hecho

--- src/api/models.py
+++ src/api/models.py
@@ -4,7 +4,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    # username = db.Column(db.String(120), unique=True, nullable=False) 
+    username = db.Column(db.String(120), unique=True, nullable=False) 
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     user_projects = db.relationship('Projects', backref='user', lazy=True)
@@ -12,15 +12,15 @@ class User(db.Model):
 
 
     def __repr__(self):
-        return '<User %r>' % self.id
-        
+        return f'<User {self.email}>'
+
 
     def serialize(self):
         return {
             "id": self.id,
             # "username": self.username,
             "email": self.email,
-            "password": self.password,
+            # "password": self.password,
             # do not serialize the password, its a security breach
         }
 
