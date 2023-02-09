d984580290d151c409f3d6ad7566df5322fe2a64
Paula <giannajuncal@gmail.com>
2023-02-09 22:23:08 +0000

    Nuevo posteo para arreglar

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -22,7 +22,8 @@ const getState = ({
             usuario: "",
             correo: "",
             contraseña1: "",
-            contraseña2: ""
+            contraseña2: "",
+            unproyecto: {}
         },
         actions: {
             // Use getActions to call a function within a fuction
@@ -32,7 +33,7 @@ const getState = ({
 
             login: (userEmail, userPassword) => {
                 fetch(
-                        "https://3001-l183r-atica-2vf2jorxvkh.ws-us86.gitpod.io/api/login", {
+                        "https://3001-l183r-atica-8f0zpiwp6rh.ws-us86.gitpod.io/api/login", {
                             method: "POST",
                             // mode: "no-cors",
                             // credentials: "include",
@@ -66,7 +67,7 @@ const getState = ({
             },
 
             signup: (userName, userPassword, userEmail) => {
-                fetch("https://3001-l183r-atica-2vf2jorxvkh.ws-us86.gitpod.io/api/signup", {
+                fetch("https://3001-l183r-atica-8f0zpiwp6rh.ws-us86.gitpod.io/api/signup", {
                         method: 'POST',
                         // mode: "no-cors",
                         // credentials: "include",
@@ -99,16 +100,58 @@ const getState = ({
                     .catch((err) => console.log(err))
             },
 
+            registrarProyecto: (postCategoria, postTitulo, postDescripción, postContacto) => {
+                fetch("https://3001-l183r-atica-8f0zpiwp6rh.ws-us86.gitpod.io/api/newproject", {
+                        method: 'POST',
+                        // mode: "no-cors",
+                        // credentials: "include",
+                        headers: {
+                            'Content-Type': 'application/json'
+                            // 'Content-Type': 'application/x-www-form-urlencoded',
+                        },
+                        body: JSON.stringify({
+                            "category": postCategoria,
+                            "title": postTitulo,
+                            "text": postDescripción,
+                            "contact": postContacto,
+                            // "project_id": postProject
+
+                        }) // body data type must match "Content-Type" header
+                    })
+                    .then((response) => {
+                        console.log(response.status);
+                        return response.json()
+                    })
+                    .then((data) => {
+                        console.log(data)
+                        localStorage.setItem("token", data.access_token)
+                    })
+                    .catch((err) => console.log(err))
+            },
+
             logout: () => {
                 localStorage.removeItem('token');
                 setStore({
                     auth: false
                 })
-            },            logout: () => {
+            },
+            logout: () => {
                 localStorage.removeItem('token');
                 setStore({
                     auth: false
                 })
+            },
+
+            obtenerId: (id) => {
+                fetch("https://3001-l183r-atica-8f0zpiwp6rh.ws-us86.gitpod.io/viewproject/" + id)
+                
+                    .then((res) => res.json())
+                    .then((data) =>
+                        setStore({
+                            unproyecto: data,
+                        })
+                    )
+                    .catch((err) => console.error(err));
             },
 
             getMessage: async () => {
