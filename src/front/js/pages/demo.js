7293112b989e814877b517331d6bdd8c932a6c53
Paula <giannajuncal@gmail.com>
2023-02-13 13:56:45 +0000

    Todo funcionando

--- src/front/js/pages/demo.js
+++ src/front/js/pages/demo.js
@@ -9,7 +9,7 @@ export const Demo = () => {
 	return (
 		<div className="container">
 			<h4 className=""> Esta view la voy a usar para cosas 
-			que posteriormente voy a tener que mover a views que todavía no están hechas</h4>
+			que posteriormente voy a tener que mover a views que todavía no están hechas.</h4>
 
 			<Link to="/nuevoposteoview">
 				<button className="btn btn-primary">New Post</button>
