922dee1595acdf80c874c2b864b8ae0129686692
NeoClean <salado2012@gmail.com>
2023-02-17 18:44:55 +0000

    Recuperacion de conteaseña back

--- src/api/routes.py
+++ src/api/routes.py
@@ -177,3 +177,45 @@ def handle_commentary_list(project_id):
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
+
+
+# recuperacion una contraseña
+
+
+from flask import Flask, request, jsonify, url_for, Blueprint, current_app #importamos current_app
+from api.models import db, User
+from api.utils import generate_sitemap, APIException
+from flask_cors import CORS
+from flask_jwt_extended import jwt_required, create_access_token
+from flask_mail import Message #importamos Message() de flask_mail
+import random #importamos ramdom y string para generar una clave aleatoria nueva
+import string
+
+
+api = Blueprint('api', __name__)
+
+
+#RECUPERACION CONTRASEÑA OLVIDADA 
+@api.route("/resetPassword", methods=["POST"])
+def resetPassword():
+    recover_email = request.json['email']
+    recover_password = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(8)) 
+    #clave aleatoria nueva
+    if not recover_email:
+        return jsonify({"msg": "Debe ingresar el correo"}), 401
+	#   #busco si el correo existe en mi base de datos
+    user = User.query.filter_by(email=recover_email).first()
+    if not user:
+    # recover_email != user.email:
+        return jsonify({"msg": "El correo ingresado no existe en nuestros registros"}), 400
+    # #si existe guardo la nueva contraseña aleatoria
+    user.password = recover_password
+    db.session.commit()
+#luego se la envio al usuario por correo para que pueda ingresar
+    
+    msg = Message("Hi", recipients=[recover_email])
+    msg.recipients=[recover_email]
+    msg.html = f"""<h1>Su nueva contraseña es: {recover_password}</h1>"""
+    current_app.mail.send(msg)
+    return jsonify({"msg": "Su nueva clave ha sido enviada al correo electrónico ingresado"}), 200
