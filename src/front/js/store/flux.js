2f426cf54ed41c9c7bb4bb3f56fec15285ebd090
L183R <liberolascuaga@gmail.com>
2023-02-19 19:34:47 +0000

    Todo bonito

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -13,7 +13,7 @@ const getState = ({
             contraseña1: "",
             contraseña2: "",
             unproyecto: {},
-            url: "https://3001-l183r-atica-9skmaenkt3t.ws-us87.gitpod.io",
+            url: "https://3001-salmon-marmot-dygbdv38kru.ws-us87.gitpod.io",
             url2: "", //url.replace("3001", "3000"),
             projects: [],
             project: {},
